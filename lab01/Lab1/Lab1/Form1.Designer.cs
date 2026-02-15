08d2f7f0de70d2a32b866f4528b5820a57df2e1b
Alexandr Lyamburtsev <48863296+Nobahu@users.noreply.github.com>
2026-02-16 00:02:16 +0700

    Rady

---
+++ lab01/Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            StartButton = new Button();
+            HeightLabel = new Label();
+            AngleLabel = new Label();
+            SpeedLabel = new Label();
+            HeightTextBox = new TextBox();
+            AngleTextBox = new TextBox();
+            SpeedTextBox = new TextBox();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            timer1 = new System.Windows.Forms.Timer(components);
+            WeightLabel = new Label();
+            SizeLabel = new Label();
+            WeightTextBox = new TextBox();
+            SizeTextBox = new TextBox();
+            DtLabel = new Label();
+            DtTextBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // StartButton
+            // 
+            StartButton.Location = new Point(770, 50);
+            StartButton.Name = "StartButton";
+            StartButton.Size = new Size(75, 23);
+            StartButton.TabIndex = 0;
+            StartButton.Text = "Запуск";
+            StartButton.UseVisualStyleBackColor = true;
+            // 
+            // HeightLabel
+            // 
+            HeightLabel.AutoSize = true;
+            HeightLabel.Location = new Point(36, 31);
+            HeightLabel.Name = "HeightLabel";
+            HeightLabel.Size = new Size(129, 15);
+            HeightLabel.TabIndex = 1;
+            HeightLabel.Text = "Начальная высота (м)";
+            // 
+            // AngleLabel
+            // 
+            AngleLabel.AutoSize = true;
+            AngleLabel.Location = new Point(36, 73);
+            AngleLabel.Name = "AngleLabel";
+            AngleLabel.Size = new Size(110, 15);
+            AngleLabel.TabIndex = 2;
+            AngleLabel.Text = "Угол броска (град)";
+            // 
+            // SpeedLabel
+            // 
+            SpeedLabel.AutoSize = true;
+            SpeedLabel.Location = new Point(36, 114);
+            SpeedLabel.Name = "SpeedLabel";
+            SpeedLabel.Size = new Size(151, 15);
+            SpeedLabel.TabIndex = 3;
+            SpeedLabel.Text = "Начальная скорость (м/с)";
+            // 
+            // HeightTextBox
+            // 
+            HeightTextBox.Location = new Point(189, 28);
+            HeightTextBox.Name = "HeightTextBox";
+            HeightTextBox.Size = new Size(100, 23);
+            HeightTextBox.TabIndex = 4;
+            // 
+            // AngleTextBox
+            // 
+            AngleTextBox.Location = new Point(189, 70);
+            AngleTextBox.Name = "AngleTextBox";
+            AngleTextBox.Size = new Size(100, 23);
+            AngleTextBox.TabIndex = 5;
+            // 
+            // SpeedTextBox
+            // 
+            SpeedTextBox.Location = new Point(189, 111);
+            SpeedTextBox.Name = "SpeedTextBox";
+            SpeedTextBox.Size = new Size(100, 23);
+            SpeedTextBox.TabIndex = 6;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(1, 151);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(1060, 490);
+            chart1.TabIndex = 7;
+            chart1.Text = "chart1";
+            // 
+            // WeightLabel
+            // 
+            WeightLabel.AutoSize = true;
+            WeightLabel.Location = new Point(346, 31);
+            WeightLabel.Name = "WeightLabel";
+            WeightLabel.Size = new Size(91, 15);
+            WeightLabel.TabIndex = 8;
+            WeightLabel.Text = "Масса тела (кг)";
+            // 
+            // SizeLabel
+            // 
+            SizeLabel.AutoSize = true;
+            SizeLabel.Location = new Point(346, 73);
+            SizeLabel.Name = "SizeLabel";
+            SizeLabel.Size = new Size(141, 15);
+            SizeLabel.TabIndex = 9;
+            SizeLabel.Text = "Площадь сечения (м^2)";
+            // 
+            // WeightTextBox
+            // 
+            WeightTextBox.Location = new Point(521, 28);
+            WeightTextBox.Name = "WeightTextBox";
+            WeightTextBox.Size = new Size(100, 23);
+            WeightTextBox.TabIndex = 10;
+            // 
+            // SizeTextBox
+            // 
+            SizeTextBox.Location = new Point(521, 70);
+            SizeTextBox.Name = "SizeTextBox";
+            SizeTextBox.Size = new Size(100, 23);
+            SizeTextBox.TabIndex = 11;
+            // 
+            // DtLabel
+            // 
+            DtLabel.AutoSize = true;
+            DtLabel.Location = new Point(346, 114);
+            DtLabel.Name = "DtLabel";
+            DtLabel.Size = new Size(154, 15);
+            DtLabel.TabIndex = 12;
+            DtLabel.Text = "Шаг моделирования (dt, c)";
+            // 
+            // DtTextBox
+            // 
+            DtTextBox.Location = new Point(521, 111);
+            DtTextBox.Name = "DtTextBox";
+            DtTextBox.Size = new Size(100, 23);
+            DtTextBox.TabIndex = 13;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(111, 644);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(288, 644);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 18;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(500, 644);
+            label3.Name = "label3";
+            label3.Size = new Size(0, 15);
+            label3.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1061, 668);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(DtTextBox);
+            Controls.Add(DtLabel);
+            Controls.Add(SizeTextBox);
+            Controls.Add(WeightTextBox);
+            Controls.Add(SizeLabel);
+            Controls.Add(WeightLabel);
+            Controls.Add(chart1);
+            Controls.Add(SpeedTextBox);
+            Controls.Add(AngleTextBox);
+            Controls.Add(HeightTextBox);
+            Controls.Add(SpeedLabel);
+            Controls.Add(AngleLabel);
+            Controls.Add(HeightLabel);
+            Controls.Add(StartButton);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button StartButton;
+        private Label HeightLabel;
+        private Label AngleLabel;
+        private Label SpeedLabel;
+        private TextBox HeightTextBox;
+        private TextBox AngleTextBox;
+        private TextBox SpeedTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private Label WeightLabel;
+        private Label SizeLabel;
+        private TextBox WeightTextBox;
+        private TextBox SizeTextBox;
+        private Label DtLabel;
+        private TextBox DtTextBox;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ lab01/Lab1/Lab1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        private void SetupChart()
+        {
+            // Названия осей
+            chart1.ChartAreas[0].AxisX.Title = "Расстояние, м";
+            chart1.ChartAreas[0].AxisY.Title = "Высота, м";
+
+            // Начальные границы
+            chart1.ChartAreas[0].AxisX.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisX.Maximum = 50;
+            chart1.ChartAreas[0].AxisY.Maximum = 30;
+
+            // Фикс шаг сетки
+            chart1.ChartAreas[0].AxisX.Interval = 10;
+            chart1.ChartAreas[0].AxisY.Interval = 5;
+        }
+
+        const decimal g = 9.81M;
+        const decimal c = 0.15M;
+        const decimal rho = 1.29M;
+        decimal dt = 0;
+        decimal t, x, y, v0, cosa, sina, S, m, k, vx, vy;
+
+        // Переменные для хранения результатов текущего полета
+        decimal maxHeight = 0;
+        decimal finalX = 0;
+        decimal finalVx = 0;
+        decimal finalVy = 0;
+
+        // Счетчик траекторий для разных цветов
+        int trajectoryCount = 0;
+
+        private Color[] colors = new Color[]
+        {
+            Color.Red,
+            Color.Blue,
+            Color.Green,
+            Color.Orange,
+            Color.Purple,
+            Color.Brown,
+            Color.Cyan,
+            Color.Magenta,
+            Color.DarkRed,
+            Color.DarkBlue
+        };
+
+        double currentMaxX = 50;
+        double currentMaxY = 30;
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(HeightTextBox.Text, out decimal height) &&
+                decimal.TryParse(SpeedTextBox.Text, out decimal speed) &&
+                decimal.TryParse(AngleTextBox.Text, out decimal angle) &&
+                decimal.TryParse(WeightTextBox.Text, out decimal weight) &&
+                decimal.TryParse(SizeTextBox.Text, out decimal size) &&
+                decimal.TryParse(DtTextBox.Text, out decimal parsedDt))
+            {
+                dt = parsedDt;
+                if (!timer1.Enabled)
+                {
+                    // Новая серия для новой траектории
+                    string seriesName = $"dt = {parsedDt}";
+
+                    Series trajectorySeries = new Series(seriesName);
+                    trajectorySeries.ChartType = SeriesChartType.Line;
+                    trajectorySeries.BorderWidth = 2;
+                    trajectorySeries.Color = colors[trajectoryCount % colors.Length];
+                    trajectorySeries.MarkerSize = 0;
+
+                    chart1.Series.Add(trajectorySeries);
+
+                    // Сбрасываем результаты текущего полета
+                    maxHeight = 0;
+                    finalX = 0;
+                    finalVx = 0;
+                    finalVy = 0;
+
+                    t = 0;
+                    x = 0;
+                    y = height;
+                    v0 = speed;
+                    double a = (double)angle * Math.PI / 180;
+                    cosa = (decimal)Math.Cos(a);
+                    sina = (decimal)Math.Sin(a);
+                    S = size;
+                    m = weight;
+                    k = 0.5M * c * rho * S / m;
+                    vx = v0 * cosa;
+                    vy = v0 * sina;
+
+                    // Добавляем первую точку в НОВУЮ траекторию
+                    chart1.Series[seriesName].Points.AddXY(x, y);
+
+                    trajectoryCount++;
+
+                    timer1.Start();
+                }
+                else
+                {
+                    timer1.Stop();
+
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректные числовые значения",
+                     "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            decimal v = (decimal)Math.Sqrt((double)(vx * vx + vy * vy));
+            vx -= k * vx * v * dt;
+            vy -= (g + k * vy * v) * dt;
+            x += vx * dt;
+            y += vy * dt;
+
+            // Отслеживаем максимальную высоту
+            if (y > maxHeight)
+            {
+                maxHeight = y;
+            }
+
+            // Проверяем и расширяем границы если надо
+            if ((double)x > currentMaxX)
+            {
+                currentMaxX = Math.Ceiling((double)x / 10) * 10 + 10;
+                chart1.ChartAreas[0].AxisX.Maximum = currentMaxX;
+                chart1.Invalidate();
+            }
+
+            if ((double)y > currentMaxY)
+            {
+                currentMaxY = Math.Ceiling((double)y / 10) * 10 + 10;
+                chart1.ChartAreas[0].AxisY.Maximum = currentMaxY;
+                chart1.Invalidate();
+            }
+
+            if (y <= 0)
+            {
+                timer1.Stop();
+
+                finalX = x;
+                finalVx = vx;
+                finalVy = vy;
+
+                decimal finalSpeed = (decimal)Math.Sqrt((double)(finalVx * finalVx + finalVy * finalVy));
+                label1.Text = $"Высота: {maxHeight:F2} м";
+                label2.Text = $"Дистанция: {finalX:F2} м";
+                label3.Text = $"Скорость: {finalSpeed:F2} м/с";
+
+                return;
+            }
+
+            chart1.Series[chart1.Series.Count - 1].Points.AddXY(x, y);
+        }
+
+        // Кнопка для очистки всех траекторий
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            while (chart1.Series.Count > 1)
+            {
+                chart1.Series.RemoveAt(1);
+            }
+
+            if (chart1.Series.Count > 0)
+            {
+                chart1.Series[0].Points.Clear();
+            }
+
+            trajectoryCount = 0;
+            currentMaxX = 50;
+            currentMaxY = 30;
+            chart1.ChartAreas[0].AxisX.Maximum = currentMaxX;
+            chart1.ChartAreas[0].AxisY.Maximum = currentMaxY;
+
+            // Очищаем лейблы
+            label1.Text = "Высота: --";
+            label2.Text = "Дистанция: --";
+            label3.Text = "Скорость: --";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupChart();
+
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
+            StartButton.Click += StartButton_Click;
+
+            Button clearButton = new Button();
+            clearButton.Text = "Очистить все";
+            clearButton.Location = new System.Drawing.Point(770, 110);
+            clearButton.Size = new System.Drawing.Size(100, 30);
+            clearButton.Click += ClearButton_Click;
+            this.Controls.Add(clearButton);
+        }
+    }
+}
